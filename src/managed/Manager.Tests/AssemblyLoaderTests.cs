2385f55a812a285f97e6266fece05309fdf32fa1
Sam Neirinck <github@samneirinck.be>
2011-06-26 14:56:43 +0200

    Added manager tests

---
+++ src/managed/Manager.Tests/AssemblyLoaderTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Cemono;
+
+namespace Manager.Tests
+{
+    [TestFixture]
+    public class AssemblyLoaderTests
+    {
+
+        public void Test()
+        {
+            var loader = new AssemblyLoader();
+        }
+    }
+}
---
+++ src/managed/Manager.Tests/ConsoleRedirectortests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Rhino.Mocks;
+using Cemono;
+
+namespace Manager.Tests
+{
+    [TestFixture]
+    public class ConsoleRedirectortests
+    {
+
+        [Test]
+        public void Log_SimpleMessage_Success()
+        {
+            string msg = "Hello log!";
+
+            var mocks = new MockRepository();
+            var logging = mocks.CreateMock<ILogging>();
+            logging.Log(msg);
+
+            mocks.ReplayAll();
+
+            var redirector = new ConsoleRedirector();
+            redirector.Logging = logging;
+            redirector.WriteLine(msg);
+
+            mocks.VerifyAll();
+        }
+    }
+}
