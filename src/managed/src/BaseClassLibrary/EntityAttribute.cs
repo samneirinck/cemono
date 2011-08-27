6b12b1d402914aa49b3bd60b76fa00f7eddbabcc
Sam Neirinck <github@samneirinck.be>
2011-08-27 16:45:28 +0200

    Added EntityAttribute.cs

---
+++ src/managed/src/BaseClassLibrary/EntityAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CryEngine
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class EntityAttribute : Attribute
+    {
+        public EntityAttribute()
+            : base()
+        {
+
+        }
+
+        public string Name { get; set; }
+        public string Category { get; set; }
+    }
+}
