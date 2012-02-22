b5fc4478026a1f40fbd4fb036e65228ae9cd0a01
Ruan PA <ruan@inkdev.net>
2012-02-22 16:51:59 +0000

    Added new CryEngine.Physics static class

---
+++ BaseClassLibrary/EntitySystem/Physics.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CryEngine
+{
+	public static class Physics
+	{
+		const string gravityCVar = "p_gravity_z";
+		public static float GravityZ
+		{
+			get { return Console.GetCVar(gravityCVar).FVal; }
+			set { Console.GetCVar(gravityCVar).FVal = value; }
+		}
+	}
+}
