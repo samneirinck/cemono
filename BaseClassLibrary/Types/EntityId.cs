b9d867356c14983978538cf8b134516ad83e73ad
Sam Neirinck <sam@samneirinck.be>
2012-03-03 21:05:49 +0100

    Update to latest  version

---
+++ BaseClassLibrary/Types/EntityId.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine.Types
+{
+    /// <summary>
+    /// Represents an entity id
+    /// </summary>
+    public struct EntityId
+    {
+        public int InternalId;
+    }
+}
