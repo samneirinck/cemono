909ea41d8130d641dbc8f645141c5384e6d73f17
Filip Lundgren <filip@inkdev.net>
2012-03-04 00:59:04 +0100

    Fixed possible crash on script reload.

--- CryBrary/Utils/AppDomainSerializer.cs
+++ CryBrary/Utils/AppDomainSerializer.cs
@@ -211,6 +211,9 @@ namespace CryEngine.Utils
 
 		static void WriteValue(object value, XmlWriter writer, string name = null)
 		{
+			if(value == null)
+				return;
+
 			if (!string.IsNullOrEmpty(name))
 				writer.WriteAttributeString("Name", name);
 
