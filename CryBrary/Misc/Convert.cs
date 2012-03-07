a29c6e3337d1a3240f6295d027eb05547bc99a7a
Filip Lundgren <filip@inkdev.net>
2012-03-07 20:46:50 +0100

    Exposed System.Convert.ChangeType within CryEngine.Math, and extended it to support Enum & Vec3 parsing.

--- CryBrary/Misc/Convert.cs
+++ CryBrary/Misc/Convert.cs
@@ -2,55 +2,39 @@
 {
 	public static class Convert
 	{
-		public static object FromString(string type, string value)
-		{
-			if(string.IsNullOrEmpty(value) || string.IsNullOrEmpty(type))
-				return null;
-
-			switch (type)
-			{
-				case "Boolean":
-					return System.Convert.ToBoolean(value);
-				case "Int32":
-					return System.Convert.ToInt32(value);
-				case "Single":
-					return System.Convert.ToSingle(value);
-				case "UInt32":
-					return System.Convert.ToUInt32(value);
-				case "String":
-					return value;
-				case "Vec3":
-					{
-						string[] split = value.Split(',');
-						return new Vec3(System.Convert.ToSingle(split[0]), System.Convert.ToSingle(split[1]), System.Convert.ToSingle(split[2]));
-					}
-			}
-
-			return null;
-		}
-
-		public static object FromString(System.Type type, string value)
-		{
-			return FromString(type.Name, value);
-		}
-
 		public static object FromString(EntityPropertyType type, string value)
 		{
 			switch (type)
-			{
+ 			{
 				case EntityPropertyType.Bool:
-					return FromString(typeof(bool), value);
+					return ChangeType(value, typeof(bool));
 				case EntityPropertyType.Int:
-					return FromString(typeof(int), value);
+					return ChangeType(value, typeof(int));
 				case EntityPropertyType.Float:
-					return FromString(typeof(float), value);
+					return ChangeType(value, typeof(float));
 				case EntityPropertyType.String:
 					return value;
 				case EntityPropertyType.Vec3:
-						return FromString(typeof(Vec3), value);
+					return ChangeType(value, typeof(Vec3));
+ 			}
+ 
+			return null;
+ 		}
+
+		public static object ChangeType(object value, System.Type conversionType)
+		{
+			var valueType = value.GetType();
+
+			if(conversionType.IsEnum && valueType == typeof(string))
+				return System.Enum.Parse(conversionType, (string)value);
+			else if(conversionType == typeof(Vec3) && valueType == typeof(string))
+			{
+				string[] split = ((string)value).Split(',');
+
+				return new Vec3(System.Convert.ToSingle(split[0]), System.Convert.ToSingle(split[1]), System.Convert.ToSingle(split[2]));
 			}
 
-			return null;
+			return System.Convert.ChangeType(value, conversionType);
 		}
 	}
 }
