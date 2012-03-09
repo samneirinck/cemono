e04b3740231b91b7a18e002d842a53257eb5c64d
Filip Lundgren <filip@inkdev.net>
2012-03-09 14:11:28 +0100

    Re-implemented RayWorldIntersection.

--- CryBrary/Engine/PhysicalWorld.cs
+++ CryBrary/Engine/PhysicalWorld.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
+using System.Linq;
+
 namespace CryEngine
 {
     public class PhysicalWorld
     {
 		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		extern internal static void _RayWorldIntersection(string gamemode);
+		extern internal static int _RayWorldIntersection(Vec3 origin, Vec3 dir, EntityQueryFlags objFlags, RayWorldIntersectionFlags flags, ref RayHit rayHit, int maxHits, object[] skipEnts);
 
 		public struct RaycastHit
 		{
@@ -18,7 +20,7 @@ namespace CryEngine
 			}
 
 			public float Distance { get { return _info.dist; } }
-			public EntityId Collider { get { return new EntityId(_info.colliderId); } }
+			public EntityId ColliderId { get { return new EntityId(_info.colliderId); } }
 			public Vec3 Point { get { return _info.pt; } }
 			public Vec3 Normal { get { return _info.n; } }
 		}
@@ -39,26 +41,6 @@ namespace CryEngine
 	        internal int iPrim; // hit triangle index
         };
 
-        [Flags]
-        public enum RayWorldIntersectionFlag
-        {
-            IgnoreTerrainHole = 0x20,
-            IgnoreNonColliding = 0x40,
-            IgnoreBackfaces = 0x80,
-            IgnoreSolidBackfaces = 0x100,
-            StopAtPierceable = 0x0F,
-            /// <summary>
-            /// among pierceble hits, materials with sf_important will have priority
-            /// </summary>
-            SeperateImportantHits = 0x200,
-            /// <summary>
-            /// if several colltype flag are specified, switches between requiring all or any of them in a geometry
-            /// </summary>
-            CollissionTypeAny = 0x400,
-            ForcePiercableNonCollidable = 0x1000, // non-colliding geometries will be treated as pierceable regardless of the actual material
-            AnyHit = 0x8000 // returns the first found hit for meshes, not necessarily the closets
-        }
-
 		/// <summary>
 		/// Steps through the entity grid and raytraces entities
 		/// traces a finite ray from org along dir
@@ -71,11 +53,110 @@ namespace CryEngine
 		/// <param name="maxHits"></param>
 		/// <param name="skipEntities"></param>
 		/// <returns>The total amount of hits detected (solid and pierceable)</returns>
-		public static int RayWorldIntersection(Vec3 origin, Vec3 dir, int objectTypes, RayWorldIntersectionFlag flags, out RaycastHit hits, int maxHits, EntityId[] skipEntities)
+		public static int RayWorldIntersection(Vec3 origin, Vec3 dir, EntityQueryFlags objectTypes, RayWorldIntersectionFlags flags, out RaycastHit hits, int maxHits, EntityId[] skipEntities)
         {
-			hits = new RaycastHit();
+			var internalRayHit = new RayHit();
 
-            return -1;
+			object[] skippedEntities = null;
+			if(skipEntities != null && skipEntities.Count() > 0)
+				skippedEntities = skipEntities.Cast<object>().ToArray(); 
+
+			int rayResult = _RayWorldIntersection(origin, dir, objectTypes, flags, ref internalRayHit, maxHits, skippedEntities);
+
+			hits = new RaycastHit(internalRayHit);
+
+            return rayResult;
         }
     }
+
+	[Flags]
+	public enum SurfaceFlags
+	{
+		PierceableMask = 0x0F,
+		MaxPierceable = 0x0F,
+		Important = 0x200,
+		ManuallyBreakable = 0x400,
+		MaterialBreakableBit = 16
+	}
+
+	[Flags]
+	public enum RayWorldIntersectionFlags
+	{
+		IgnoreTerrainHole = 0x20,
+		IgnoreNonColliding = 0x40,
+		IgnoreBackfaces = 0x80,
+		IgnoreSolidBackfaces = 0x100,
+		PierceabilityMask = 0x0F,
+		Pierceabiltiy = 0,
+		StopAtPierceable = 0x0F,
+		/// <summary>
+		/// among pierceble hits, materials with sf_important will have priority
+		/// </summary>
+		SeperateImportantHits = SurfaceFlags.Important,
+		/// <summary>
+		/// used to manually specify collision geometry types (default is geom_colltype_ray)
+		/// </summary>
+		CollissionTypeBit = 16,
+		/// <summary>
+		/// if several colltype flag are specified, switches between requiring all or any of them in a geometry
+		/// </summary>
+		CollissionTypeAny = 0x400,
+		/// <summary>
+		/// queues the RWI request, when done it'll generate EventPhysRWIResult
+		/// </summary>
+		Queue = 0x800,
+		/// <summary>
+		/// non-colliding geometries will be treated as pierceable regardless of the actual material
+		/// </summary>
+		ForcePiercableNonCollidable = 0x1000,
+		/// <summary>
+		/// marks the rwi to be a debug rwi (used for spu debugging, only valid in non-release builds)
+		/// </summary>
+		DebugTrace = 0x2000,
+		/// <summary>
+		/// update phitLast with the current hit results (should be set if the last hit should be reused for a "warm" start)
+		/// </summary>
+		UpdateLastHit = 0x4000,
+		/// <summary>
+		/// returns the first found hit for meshes, not necessarily the closest
+		/// </summary>
+		AnyHit = 0x8000
+	}
+
+	/// <summary>
+	/// Used for GetEntitiesInBox and RayWorldIntersection
+	/// </summary>
+	[Flags]
+	public enum EntityQueryFlags
+	{
+		Static = 1, SleepingRigid = 2, Rigid = 4, Living = 8, Independent = 16, Deleted = 128, Terrain = 0x100,
+		All = Static | SleepingRigid | Rigid | Living | Independent | Terrain,
+		FlaggedOnly = 0x800, SkipFlagged = FlaggedOnly * 2, // "flagged" meas has pef_update set
+		Areas = 32, Triggers = 64,
+		IgnoreNonColliding = 0x10000,
+		/// <summary>
+		/// sort by mass in ascending order
+		/// </summary>
+		SortByMass = 0x20000,
+		/// <summary>
+		/// if not set, the function will return an internal pointer
+		/// </summary>
+		AllocateList = 0x40000,
+		/// <summary>
+		/// will call AddRef on each entity in the list (expecting the caller call Release)
+		/// </summary>
+		AddRefResults = 0x100000,
+		/// <summary>
+		/// can only be used in RayWorldIntersection
+		/// </summary>
+		Water = 0x200,
+		/// <summary>
+		/// can only be used in RayWorldIntersection
+		/// </summary>
+		NoOnDemandActivation = 0x80000,
+		/// <summary>
+		/// queues procedural breakage requests; can only be used in SimulateExplosion
+		/// </summary>
+		DelayedDeformations = 0x80000
+	}
 }
--- MonoDll/Scriptbinds/ScriptBind_PhysicalWorld.cpp
+++ MonoDll/Scriptbinds/ScriptBind_PhysicalWorld.cpp
@@ -11,20 +11,31 @@ CScriptBind_PhysicalWorld::CScriptBind_PhysicalWorld()
 
 int CScriptBind_PhysicalWorld::RayWorldIntersection(Vec3 origin, Vec3 dir, int objFlags, unsigned int flags, MonoRayHit &hit, int maxHits, mono::array skipEntities)
 {
+	IPhysicalEntity **pSkipEnts = NULL;
+	IMonoArray *pSkipEntities = NULL;
+
+	if(skipEntities != NULL)
+	{
+		pSkipEntities = *skipEntities;
+		pSkipEnts = new IPhysicalEntity*[pSkipEntities->GetSize()];
+		for(int i = 0; i < pSkipEntities->GetSize(); i++)
+			pSkipEnts[i] = gEnv->pPhysicalWorld->GetPhysicalEntityById(pSkipEntities->GetItem(i)->Unbox<EntityId>());
+
+		delete pSkipEntities;
+	}
+
 	ray_hit realHit;
 
-	IMonoArray *pSkipEntities = *skipEntities;
-	IPhysicalEntity **pSkipEnts = new IPhysicalEntity*[pSkipEntities->GetSize()];
-	for(int i = 0; i < pSkipEntities->GetSize(); i++)
-		pSkipEnts[i] = gEnv->pPhysicalWorld->GetPhysicalEntityById(pSkipEntities->GetItem(i)->Unbox<EntityId>());
+	int numHits = gEnv->pPhysicalWorld->RayWorldIntersection(origin, dir, objFlags, flags, &realHit, maxHits, pSkipEntities ? pSkipEnts : NULL, pSkipEntities ? pSkipEntities->GetSize() : 0);
 
-	int numHits = gEnv->pPhysicalWorld->RayWorldIntersection(origin, dir, objFlags, flags, &realHit, maxHits, pSkipEnts);
-
-	delete[] pSkipEnts;
-	delete pSkipEntities;
+	SAFE_DELETE_ARRAY(pSkipEnts);
 
 	hit.bTerrain = realHit.bTerrain;
-	hit.colliderId = gEnv->pPhysicalWorld->GetPhysicalEntityId(realHit.pCollider);
+
+	// We should return physical entity id's really, but this isn't exposed yet.
+	if(realHit.pCollider != NULL && realHit.pCollider->GetType() != PE_STATIC && realHit.pCollider->GetType() != PE_NONE)
+		hit.colliderId = gEnv->pPhysicalWorld->GetPhysicalEntityId(realHit.pCollider);
+
 	hit.dist = realHit.dist;
 	hit.foreignIdx = realHit.foreignIdx;
 	hit.idmatOrg = realHit.idmatOrg;
