20b555aa072c560f20e14f36dcd7341c8073a496
Filip Lundgren <filip@inkdev.net>
2012-04-30 21:52:39 +0200

    Fix: CScriptArray::InsertArray ignoring NULL objects.

--- MonoDll/MonoArray.h
+++ MonoDll/MonoArray.h
@@ -35,7 +35,7 @@ public:
 	virtual IMonoArray *GetItemArray(int index) override { return new CScriptArray((mono::array)mono_array_get((MonoArray *)m_pArray, MonoArray *, index)); }
 
 	virtual void Insert(IMonoObject *pObject) override;
-	virtual void Insert(IMonoArray *pArray) override { pArray ? InsertArray(*pArray) : 0; }
+	virtual void Insert(IMonoArray *pArray) override { InsertArray(pArray ? *pArray : (mono::array)NULL); }
 
 	virtual void InsertString(mono::string string) override;
 	virtual void InsertObject(mono::object obj) override;
