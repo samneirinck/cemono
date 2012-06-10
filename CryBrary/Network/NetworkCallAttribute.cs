525f72ca81c346935ae4ef3b0a46626514a83a0c
Filip Lundgren <filip@inkdev.net>
2012-06-10 17:44:01 +0200

    Added NetworkCallAttribute.

---
+++ CryBrary/Network/NetworkCallAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace CryEngine
+{
+	[AttributeUsage(AttributeTargets.Method)]
+	public sealed class NetworkCallAttribute : Attribute
+	{
+		public NetworkCallAttribute(NetworkTarget target)
+		{
+			Target = target;
+		}
+
+		public NetworkTarget Target;
+	}
+
+	public enum NetworkTarget
+	{
+		Any,
+		Server,
+		Client
+	}
+}
