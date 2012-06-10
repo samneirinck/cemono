ac5532dd241c909d2d92f6c0feb89af9d89680ed
Ruan PA <ruan@inkdev.net>
2012-06-10 02:07:39 +0100

    Added basic GUI report for unit testing

--- CryBrary/Unit Testing/Reports/ReportForm.Designer.cs
+++ CryBrary/Unit Testing/Reports/ReportForm.Designer.cs
@@ -29,17 +29,31 @@ namespace CryEngine.Testing.Internals
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportForm));
 			this.uxTestTree = new System.Windows.Forms.TreeView();
+			this.uxTreeImages = new System.Windows.Forms.ImageList(this.components);
 			this.SuspendLayout();
 			// 
 			// uxTestTree
 			// 
 			this.uxTestTree.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
+			this.uxTestTree.ImageIndex = 0;
+			this.uxTestTree.ImageList = this.uxTreeImages;
 			this.uxTestTree.Location = new System.Drawing.Point(12, 12);
 			this.uxTestTree.Name = "uxTestTree";
+			this.uxTestTree.SelectedImageIndex = 0;
 			this.uxTestTree.Size = new System.Drawing.Size(382, 458);
 			this.uxTestTree.TabIndex = 0;
+			// 
+			// uxTreeImages
+			// 
+			this.uxTreeImages.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("uxTreeImages.ImageStream")));
+			this.uxTreeImages.TransparentColor = System.Drawing.Color.Transparent;
+			this.uxTreeImages.Images.SetKeyName(0, "TestSuccess.png");
+			this.uxTreeImages.Images.SetKeyName(1, "TestFailed.png");
+			this.uxTreeImages.Images.SetKeyName(2, "TestIgnored.png");
 			// 
 			// ReportForm
 			// 
@@ -49,6 +63,7 @@ namespace CryEngine.Testing.Internals
 			this.Controls.Add(this.uxTestTree);
 			this.Name = "ReportForm";
 			this.Text = "CryMono Unit Test Report";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.OnClose);
 			this.ResumeLayout(false);
 
 		}
@@ -56,5 +71,6 @@ namespace CryEngine.Testing.Internals
 		#endregion
 
 		private System.Windows.Forms.TreeView uxTestTree;
+		private System.Windows.Forms.ImageList uxTreeImages;
 	}
 }
