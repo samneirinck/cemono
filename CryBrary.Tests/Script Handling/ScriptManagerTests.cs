5ffd800f29b6382a2cc8a5a0e84a7379fec29387
Sam Neirinck <github@samneirinck.be>
2012-06-30 15:23:53 +0200

    Added first ScriptManager test

---
+++ CryBrary.Tests/Script Handling/ScriptManagerTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using CryEngine.Initialization;
+using NUnit.Framework;
+
+namespace CryBrary.Tests.Script_Handling
+{
+    [TestFixture]
+    public class ScriptManagerTests : CryBraryTests
+    {
+        [Test]
+        public void ConstructAndInitialize_ScriptManager_WithoutExceptions()
+        {
+            ScriptManager manager = new ScriptManager();
+
+            manager.PostInit();
+        }
+    }
+}
