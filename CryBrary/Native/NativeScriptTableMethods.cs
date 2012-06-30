31081dfa6fd747af222a3908078e9b26442135cc
Sam Neirinck <github@samneirinck.be>
2012-06-30 23:04:24 +0200

    Added native scripttable methods

---
+++ CryBrary/Native/NativeScriptTableMethods.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using CryEngine.Lua;
+
+namespace CryEngine.Native
+{
+    internal class NativeScriptTableMethods : INativeScriptTableMethods
+    {
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern internal static IntPtr _GetScriptTable(EntityId entityId);
+
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern internal static object _CallMethod(IntPtr scriptTable, string methodName, LuaVariableType returnType, object[] args);
+        [MethodImplAttribute(MethodImplOptions.InternalCall)]
+        extern internal static void _CallMethodVoid(IntPtr scriptTable, string methodName, object[] args);
+
+
+        public IntPtr GetScriptTable(EntityId entityId)
+        {
+            return _GetScriptTable(entityId);
+        }
+
+        public object CallMethod(IntPtr scriptTable, string methodName, Lua.LuaVariableType returnType, object[] args)
+        {
+            return _CallMethod(scriptTable, methodName, returnType, args);
+        }
+
+        public void CallMethodVoid(IntPtr scriptTable, string methodName, object[] args)
+        {
+            _CallMethodVoid(scriptTable, methodName, args);
+        }
+    }
+}
