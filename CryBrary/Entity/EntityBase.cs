c0e09530855cae424f2ad7ae1bbbe20935734f02
Sam Neirinck <github@samneirinck.be>
2012-06-30 22:46:32 +0200

    Added native entity methods

--- CryBrary/Entity/EntityBase.cs
+++ CryBrary/Entity/EntityBase.cs
@@ -6,21 +6,16 @@ using CryEngine.Native;
 
 namespace CryEngine
 {
-	internal interface INativeEntityMethods
-	{
-        bool RegisterClass(EntityRegistrationParams config);
-	}
-
 	/// <summary>
 	/// Contains common entity functionality.
 	/// </summary>
 	public abstract class EntityBase : CryScriptInstance
 	{
-		private static INativeEntityMethods _methods;
-		internal static INativeEntityMethods Methods
+		private static INativeEntityMethods _nativeEntityMethods;
+		internal static INativeEntityMethods NativeEntityMethods
 		{
-			get { return _methods ?? (_methods = new EntityMethods()); }
-			set { _methods = value; }
+			get { return _nativeEntityMethods ?? (_nativeEntityMethods = new NativeEntityMethods()); }
+			set { _nativeEntityMethods = value; }
 		}
 
         private static INativePhysicsMethods _nativePhysicsMethods;
@@ -30,121 +25,7 @@ namespace CryEngine
             set { _nativePhysicsMethods = value; }
         }
 
-		class EntityMethods : INativeEntityMethods
-		{
-			/// <summary>
-			/// Register a new entity type.
-			/// </summary>
-			/// <param name="config">The Entity configuration.</param>
-			/// <returns>True if registration succeeded, otherwise false.</returns>
-            public bool RegisterClass(EntityRegistrationParams config)
-			{
-				return _RegisterEntityClass(config);
-			}
-		}
-
 		#region Externals
-		/// <summary>
-		/// Spawns an entity with the specified parameters.
-		/// </summary>
-		/// <param name="spawnParams">The <see cref="EntitySpawnParams"/></param>
-		/// <param name="autoInit">Should the entity automatically be initialized?</param>
-		/// <returns></returns>
-		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		extern internal static bool _SpawnEntity(EntitySpawnParams spawnParams, bool autoInit, out EntityInfo entityInfo);
-		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		extern internal static void _RemoveEntity(uint entityId);
-
-		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-		extern internal static IntPtr _GetEntity(uint entityId);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static uint _FindEntity(string name);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-        extern internal static object[] _GetEntitiesByClass(string className);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static object[] _GetEntitiesInBox(BoundingBox bbox, EntityQueryFlags flags);
-
-		[MethodImplAttribute(MethodImplOptions.InternalCall)]
-        extern internal static bool _RegisterEntityClass(EntityRegistrationParams registerParams);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _BreakIntoPieces(IntPtr ptr, int slot, int piecesSlot, BreakageParameters breakageParams);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static string _GetName(IntPtr ptr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetName(IntPtr ptr, string name);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static EntityFlags _GetFlags(IntPtr ptr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetFlags(IntPtr ptr, EntityFlags name);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _AddMovement(IntPtr ptr, ref EntityMovementRequest request);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetWorldTM(IntPtr ptr, Matrix34 tm);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static Matrix34 _GetWorldTM(IntPtr ptr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetLocalTM(IntPtr ptr, Matrix34 tm);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static Matrix34 _GetLocalTM(IntPtr ptr);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static BoundingBox _GetWorldBoundingBox(IntPtr ptr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static BoundingBox _GetBoundingBox(IntPtr ptr);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static EntitySlotFlags _GetSlotFlags(IntPtr ptr, int slot);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetSlotFlags(IntPtr ptr, int slot, EntitySlotFlags slotFlags);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetWorldPos(IntPtr ptr, Vec3 newPos);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static Vec3 _GetWorldPos(IntPtr ptr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetPos(IntPtr ptr, Vec3 newPos);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static Vec3 _GetPos(IntPtr ptr);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetWorldRotation(IntPtr ptr, Quat newAngles);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static Quat _GetWorldRotation(IntPtr ptr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetRotation(IntPtr ptr, Quat newAngles);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static Quat _GetRotation(IntPtr ptr);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _LoadObject(IntPtr ptr, string fileName, int slot);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static string _GetStaticObjectFilePath(IntPtr ptr, int slot);
-
-		/// <summary>
-		/// Loads an non-static model on the object (.chr, .cdf, .cga)
-		/// </summary>
-		/// <param name="ptr"></param>
-		/// <param name="fileName"></param>
-		/// <param name="slot"></param>
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _LoadCharacter(IntPtr ptr, string fileName, int slot);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static int _GetAttachmentCount(IntPtr entPtr);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static IntPtr _GetAttachmentMaterialByIndex(IntPtr entPtr, int index);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static void _SetAttachmentMaterialByIndex(IntPtr entPtr, int index, IntPtr materialPtr);
-
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static IntPtr _GetAttachmentMaterial(IntPtr entPtr, string name);
-		[MethodImpl(MethodImplOptions.InternalCall)]
-		extern internal static int _SetAttachmentMaterial(IntPtr entPtr, string name, IntPtr materialPtr);
 		#endregion
 
 		#region Callbacks
@@ -156,12 +37,12 @@ namespace CryEngine
 
 		public EntitySlotFlags GetSlotFlags(int slot = 0)
 		{
-			return _GetSlotFlags(EntityPointer, slot);
+            return NativeEntityMethods.GetSlotFlags(EntityPointer, slot);
 		}
 
 		public void SetSlotFlags(EntitySlotFlags flags, int slot = 0)
 		{
-			_SetSlotFlags(EntityPointer, slot, flags);
+            NativeEntityMethods.SetSlotFlags(EntityPointer, slot, flags);
 		}
 
 		public Vec3 Velocity
@@ -179,7 +60,7 @@ namespace CryEngine
 
         internal override void OnScriptReloadInternal()
 		{
-			EntityPointer = _GetEntity(Id);
+            EntityPointer = NativeEntityMethods.GetEntity(Id);
 
 			Physics.OnScriptReload();
             base.OnScriptReloadInternal();
@@ -189,75 +70,75 @@ namespace CryEngine
 #region Attachments
 		public Material GetAttachmentMaterial(int index)
 		{
-			var ptr = _GetAttachmentMaterialByIndex(EntityPointer, index);
+            var ptr = NativeEntityMethods.GetAttachmentMaterialByIndex(EntityPointer, index);
 
 			return Material.TryAdd(ptr);
 		}
 
 		public Material GetAttachmentMaterial(string attachmentName)
 		{
-			var ptr = _GetAttachmentMaterial(EntityPointer, attachmentName);
+            var ptr = NativeEntityMethods.GetAttachmentMaterial(EntityPointer, attachmentName);
 
 			return Material.TryAdd(ptr);
 		}
 
 		public void SetAttachmentMaterial(int index, Material newMaterial)
 		{
-			_SetAttachmentMaterialByIndex(EntityPointer, index, newMaterial.MaterialPointer);
+            NativeEntityMethods.SetAttachmentMaterialByIndex(EntityPointer, index, newMaterial.MaterialPointer);
 		}
 
 		public void SetAttachmentMaterial(string name, Material newMaterial)
 		{
-			_SetAttachmentMaterial(EntityPointer, name, newMaterial.MaterialPointer);
+            NativeEntityMethods.SetAttachmentMaterial(EntityPointer, name, newMaterial.MaterialPointer);
 		}
 
-		public int AttachmentCount { get { return _GetAttachmentCount(EntityPointer); } }
+        public int AttachmentCount { get { return NativeEntityMethods.GetAttachmentCount(EntityPointer); } }
 #endregion
 
 		/// <summary>
 		/// Sets / gets the world space entity position.
 		/// </summary>
-		public Vec3 Position { get { return _GetWorldPos(EntityPointer); } set { _SetWorldPos(EntityPointer, value); } }
+        public Vec3 Position { get { return NativeEntityMethods.GetWorldPos(EntityPointer); } set { NativeEntityMethods.SetWorldPos(EntityPointer, value); } }
 		/// <summary>
 		/// Sets / gets the world space entity orientation quaternion.
 		/// </summary>
-		public Quat Rotation { get { return _GetWorldRotation(EntityPointer); } set { _SetWorldRotation(EntityPointer, value); } }
+        public Quat Rotation { get { return NativeEntityMethods.GetWorldRotation(EntityPointer); } set { NativeEntityMethods.SetWorldRotation(EntityPointer, value); } }
 
 		/// <summary>
 		/// Sets / gets the local space entity position.
 		/// </summary>
-		public Vec3 LocalPosition { get { return _GetPos(EntityPointer); } set { _SetPos(EntityPointer, value); } }
+        public Vec3 LocalPosition { get { return NativeEntityMethods.GetPos(EntityPointer); } set { NativeEntityMethods.SetPos(EntityPointer, value); } }
 		/// <summary>
 		/// Sets / gets the local space entity orientation quaternion.
 		/// </summary>
-		public Quat LocalRotation { get { return _GetRotation(EntityPointer); } set { _SetRotation(EntityPointer, value); } }
+        public Quat LocalRotation { get { return NativeEntityMethods.GetRotation(EntityPointer); } set { NativeEntityMethods.SetRotation(EntityPointer, value); } }
 
 		/// <summary>
 		/// Sets / gets the world space entity transformation matrix.
 		/// </summary>
-		public Matrix34 Transform { get { return _GetWorldTM(EntityPointer); } set { _SetWorldTM(EntityPointer, value); } }
+        public Matrix34 Transform { get { return NativeEntityMethods.GetWorldTM(EntityPointer); } set { NativeEntityMethods.SetWorldTM(EntityPointer, value); } }
 		/// <summary>
 		/// Sets / gets the local space entity transformation matrix.
 		/// </summary>
-		public Matrix34 LocalTransform { get { return _GetLocalTM(EntityPointer); } set { _SetLocalTM(EntityPointer, value); } }
+        public Matrix34 LocalTransform { get { return NativeEntityMethods.GetLocalTM(EntityPointer); } set { NativeEntityMethods.SetLocalTM(EntityPointer, value); } }
 
 		/// <summary>
 		/// Gets the entity axis aligned bounding box in the world space.
 		/// </summary>
-		public BoundingBox BoundingBox { get { return _GetWorldBoundingBox(EntityPointer); } }
+        public BoundingBox BoundingBox { get { return NativeEntityMethods.GetWorldBoundingBox(EntityPointer); } }
 		/// <summary>
 		/// Gets the entity axis aligned bounding box in the world space.
 		/// </summary>
-		public BoundingBox LocalBoundingBox { get { return _GetBoundingBox(EntityPointer); } }
+        public BoundingBox LocalBoundingBox { get { return NativeEntityMethods.GetBoundingBox(EntityPointer); } }
 
 		/// <summary>
 		/// Gets / sets the entity name.
 		/// </summary>
-		public string Name { get { return _GetName(EntityPointer); } set { _SetName(EntityPointer, value); } }
+        public string Name { get { return NativeEntityMethods.GetName(EntityPointer); } set { NativeEntityMethods.SetName(EntityPointer, value); } }
 		/// <summary>
 		/// Gets / sets the entity flags.
 		/// </summary>
-		public EntityFlags Flags { get { return _GetFlags(EntityPointer); } set { _SetFlags(EntityPointer, value); } }
+        public EntityFlags Flags { get { return NativeEntityMethods.GetFlags(EntityPointer); } set { NativeEntityMethods.SetFlags(EntityPointer, value); } }
 
 		public Material Material { get { return Material.Get(this); } set { Material.Set(this, value); } }
 
