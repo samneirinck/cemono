ea27d20e2cafb9f00964ba6065c154cd9632464d
Sam Neirinck <github@samneirinck.be>
2012-06-30 22:03:41 +0200

    Added native particle effect methods

---
+++ CryBrary/Native/INativeParticleEffectMethods.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CryEngine.Native
+{
+    internal interface INativeParticleEffectMethods
+    {
+        IntPtr FindEffect(string effectName, bool loadResources);
+        void Spawn(IntPtr ptr, bool independent, Vec3 pos, Vec3 dir, float scale);
+
+    }
+}
