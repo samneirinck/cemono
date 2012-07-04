e220ac0ed6979b617af4fd830bdb1509dc8de113
Filip Lundgren <filip@inkdev.net>
2012-07-04 22:20:21 +0200

    Added WIP script reload form

---
+++ CryBrary/Script Handling/ScriptReloadMessage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Diagnostics;
+using System.CodeDom.Compiler;
+
+namespace CryEngine.Script_Handling
+{
+	public partial class ScriptReloadMessage : Form
+	{
+		public ScriptReloadMessage(CompilerResults compilationResults, bool canRevert)
+		{
+			InitializeComponent();
+
+			//tryAgainButton.Click += (s, a) => ;
+			//revertButton.Click += (s, a) => ;
+			exitButton.Click += (s, a) => Process.GetCurrentProcess().Kill();
+
+			if (!canRevert)
+				revertButton.Enabled = false;
+
+			string compilationError = string.Format("Compilation failed; {0} errors: ", compilationResults.Errors.Count);
+
+			foreach (CompilerError error in compilationResults.Errors)
+			{
+				compilationError += Environment.NewLine;
+
+				if (!error.ErrorText.Contains("(Location of the symbol related to previous error)"))
+					compilationError += string.Format("{0}({1},{2}): {3} {4}: {5}", error.FileName, error.Line, error.Column, error.IsWarning ? "warning" : "error", error.ErrorNumber, error.ErrorText);
+				else
+					compilationError += "	" + error.ErrorText;
+			}
+
+			errorBox.Text = compilationError;
+		}
+	}
+}
