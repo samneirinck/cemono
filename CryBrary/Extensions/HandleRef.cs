19e1ce84c0f4afa906f4399c4ab614a14a12b17f
Filip Lundgren <filip@inkdev.net>
2012-09-02 00:24:43 +0200

    Added HandleRef.IsValid method

---
+++ CryBrary/Extensions/HandleRef.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace CryEngine.Extensions
+{
+	public static class HandleRefExtensions
+	{
+		public static bool IsValid(this HandleRef handleRef)
+		{
+			return handleRef.Handle == IntPtr.Zero;
+		}
+	}
+}
