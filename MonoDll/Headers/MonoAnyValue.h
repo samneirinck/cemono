af992f6961fb94e31799ef1bf8c0e37f2b1f3ece
Filip Lundgren <filip@inkdev.net>
2012-09-03 01:18:33 +0200

    MOnoAnyValue: implement ISerializable.

--- MonoDll/Headers/MonoAnyValue.h
+++ MonoDll/Headers/MonoAnyValue.h
@@ -38,7 +38,7 @@ enum EMonoAnyType
 /// <summary>
 /// Simple class used to easily convert common C++ types to their C# equivalents.
 /// </summary>
-struct MonoAnyValue
+struct MonoAnyValue : public ISerializable
 {
 	MonoAnyValue() : type(eMonoAnyType_Unknown) { };
 	MonoAnyValue(bool value) : type(eMonoAnyType_Boolean) { b = value; }
@@ -47,10 +47,48 @@ struct MonoAnyValue
 	MonoAnyValue(short value) : type(eMonoAnyType_Short) { i = value; }
 	MonoAnyValue(unsigned short value) : type(eMonoAnyType_UnsignedShort) { u = value; }
 	MonoAnyValue(float value) : type(eMonoAnyType_Float) { f = value; }
-	MonoAnyValue(const char *value) : type(eMonoAnyType_String) { str = value; }
-	MonoAnyValue(string value) : type(eMonoAnyType_String) { str = value.c_str(); }
+	MonoAnyValue(const char *value) : type(eMonoAnyType_String) { cchar = value; }
+	MonoAnyValue(string value) : type(eMonoAnyType_String) { cchar = value.c_str(); }
 	MonoAnyValue(Vec3 value) : type(eMonoAnyType_Vec3) { vec3.x = value.x; vec3.y = value.y; vec3.z = value.z; }
 	MonoAnyValue(Ang3 value) : type(eMonoAnyType_Vec3) { vec3.x = value.x; vec3.y = value.y; vec3.z = value.z; }
+
+	virtual void SerializeWith(TSerialize ser) override
+	{
+		switch(type)
+		{
+		case eMonoAnyType_Boolean:
+			ser.Value("boolean", b, 'bool');
+			break;
+		case eMonoAnyType_UnsignedInteger:
+		case eMonoAnyType_UnsignedShort:
+			ser.Value("uint", u, 'ui32');
+			break;
+		case eMonoAnyType_Integer:
+		case eMonoAnyType_Short:
+			ser.Value("int", i, 'i32');
+			break;
+		case eMonoAnyType_Float:
+			ser.Value("float", f);
+			break;
+		case eMonoAnyType_Vec3:
+			ser.Value("vec", Vec3(vec3.x, vec3.y, vec3.z));
+			break;
+		case eMonoAnyType_String:
+			{
+				if(ser.IsReading())
+				{
+					str = cchar;
+					ser.Value("str", str); 
+				}
+				else
+				{
+					ser.Value("str", str);
+					cchar = str.c_str();
+				}
+			}
+			break;
+		}
+	}
 
 	void *GetValue()
 	{
@@ -69,7 +107,7 @@ struct MonoAnyValue
 		case eMonoAnyType_Vec3:
 			return Vec3(vec3.x, vec3.y, vec3.z);
 		case eMonoAnyType_String:
-			return &str;
+			return &cchar;
 		}
 	}
 
@@ -80,9 +118,11 @@ struct MonoAnyValue
 		float			f;
 		int				i;
 		unsigned int	u;
-		const char*		str;
+		const char*		cchar;
 		struct { float x,y,z; } vec3;
 	};
+
+	string str;
 };
 
 #endif //__MONO_ANY_VALUE__
