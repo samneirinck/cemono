ff847cbe1b0ba7065b8c81ad3c9e201ccf0f80a6
Filip Lundgren <filip@inkdev.net>
2012-09-05 01:59:31 +0200

    Assembly version -> 0.5.*

--- CryBrary/Properties/AssemblyInfo.cs
+++ CryBrary/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("CryBrary")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ink Studios")]
+[assembly: AssemblyCompany("Ink Studios Ltd.")]
 [assembly: AssemblyProduct("CryMono")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
@@ -33,8 +33,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.5.*")]
 
 [assembly: CLSCompliant(true)]
 [assembly: InternalsVisibleTo("CryBrary.Tests")]
