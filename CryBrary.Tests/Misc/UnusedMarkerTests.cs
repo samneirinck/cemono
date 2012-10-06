24e5ebf931fc7deee8a44593e401aeaa0065c7d5
Filip Lundgren <filip@inkdev.net>
2012-10-06 20:07:57 +0200

    Added Utils.UnusedMarker

---
+++ CryBrary.Tests/Misc/UnusedMarkerTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using NUnit.Framework;
+
+using CryEngine.Utils;
+
+namespace CryBrary.Tests.Misc
+{
+	[TestFixture]
+	public class UnusedMarkerTests
+	{
+		[Test]
+		public void Float()
+		{
+			float myFloat = UnusedMarker.Float;
+			Assert.IsTrue(UnusedMarker.IsUnused(myFloat));
+
+			myFloat = 998.4f;
+			Assert.IsFalse(UnusedMarker.IsUnused(myFloat));
+		}
+
+		[Test]
+		public void Integer()
+		{
+			int myInt = UnusedMarker.Integer;
+			Assert.IsTrue(UnusedMarker.IsUnused(myInt));
+
+			myInt = 5;
+			Assert.IsFalse(UnusedMarker.IsUnused(myInt));
+		}
+
+		[Test]
+		public void UnsignedInteger()
+		{
+			uint myUint = UnusedMarker.UnsignedInteger;
+			Assert.IsTrue(UnusedMarker.IsUnused(myUint));
+
+			myUint = 51;
+			Assert.IsFalse(UnusedMarker.IsUnused(myUint));
+		}
+	}
+}
---
+++ CryBrary/Utils/UnusedMarker.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Runtime.InteropServices;
+
+namespace CryEngine.Utils
+{
+	public static class UnusedMarker
+	{
+		[StructLayout(LayoutKind.Explicit)]
+		struct f2i
+		{
+			[FieldOffset(0)]
+			public float f;
+
+			[FieldOffset(0)]
+			public UInt32 i;
+		}
+
+		[StructLayout(LayoutKind.Explicit)]
+		struct d2i
+		{
+			[FieldOffset(0)]
+			public double d;
+
+			[FieldOffset(0)]
+			public UInt32 i;
+		}
+
+		public static float Float
+		{
+			get
+			{
+				var u = new f2i();
+				u.i = 0xFFBFFFFF;
+
+				return u.f;
+			}
+		}
+
+		public static bool IsUnused(float var)
+		{
+			var u = new f2i();
+			u.f = var;
+			return (u.i & 0xFFA00000) == 0xFFA00000;
+		}
+
+		public static int Integer
+		{
+			get
+			{
+				return 1 << 31;
+			}
+		}
+
+		public static bool IsUnused(int var)
+		{
+			return var == 1 << 31;
+		}
+
+		[CLSCompliant(false)]
+		public static uint UnsignedInteger
+		{
+			get
+			{
+				return 1u << 31;
+			}
+		}
+
+		[CLSCompliant(false)]
+		public static bool IsUnused(uint var)
+		{
+			return var == 1u << 31;
+		}
+	}
+}
