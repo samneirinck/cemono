519f165fd3eb6b56eb3df3e0275385bc1e34a429
Filip Lundgren <filip@inkdev.net>
2012-10-09 14:04:54 +0200

    Added CryEngine.Misc.BoredAtWorkException.

---
+++ CryBrary/Misc/BoredAtWorkException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine.Misc
+{
+	public class BoredAtWorkException : Exception
+	{
+		public BoredAtWorkException()
+			: base("Work boredom detected, please carry on with CryMono work")
+		{
+		}
+	}
+}
