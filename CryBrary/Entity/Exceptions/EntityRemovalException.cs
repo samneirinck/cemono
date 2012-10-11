56e7e913de27b25dadc5b26ba18a38e35962ad6f
Filip Lundgren <filip@inkdev.net>
2012-10-11 20:03:23 +0200

    Entity.Remove: Throw new EntityRemovalException if the entity was placed from the Editor.

---
+++ CryBrary/Entity/Exceptions/EntityRemovalException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine
+{
+	public class EntityRemovalException : Exception
+	{
+		public EntityRemovalException()
+		{
+		}
+
+		public EntityRemovalException(string message)
+			: base(message)
+		{
+		}
+
+		public EntityRemovalException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
