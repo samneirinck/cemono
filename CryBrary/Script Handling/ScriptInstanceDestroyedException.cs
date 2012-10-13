8c960fa09d6ff1378f6c2dc9fd95e6e336bb70d9
Filip Lundgren <filip@inkdev.net>
2012-10-13 02:13:35 +0200

    Renamed EntityDestroyedException to ScriptInstanceDestroyedException.

---
+++ CryBrary/Script Handling/ScriptInstanceDestroyedException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine
+{
+	/// <summary>
+	/// Thrown when attempting to invoke native methods on a destroyed entity.
+	/// See <see cref="CryEngine.CryScriptInstance.IsDestroyed"/>
+	/// </summary>
+	public class ScriptInstanceDestroyedException : Exception
+	{
+		public ScriptInstanceDestroyedException()
+		{
+		}
+
+		public ScriptInstanceDestroyedException(string message)
+			: base(message)
+		{
+		}
+
+		public ScriptInstanceDestroyedException(string message, Exception inner)
+			: base(message, inner)
+		{
+		}
+	}
+}
