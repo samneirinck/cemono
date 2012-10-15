5253de1136e1a25b6c52f90fd869b47a37cb94f7
Filip Lundgren <filip@inkdev.net>
2012-10-15 03:32:15 +0200

    CActor: Don't override GetRMIBase.

--- MonoDll/Actor.h
+++ MonoDll/Actor.h
@@ -154,7 +154,6 @@ public:
 	virtual void HandleEvent( const SGameObjectEvent& event ) override;
 	virtual void SetChannelId(uint16 id) override {}
 	virtual void SetAuthority( bool auth ) override;
-	virtual const void *GetRMIBase() const override { return nullptr; }
 	virtual void PostUpdate( float frameTime ) override {}
 	virtual void PostRemoteSpawn() override {}
 	// ~IGameObjectExtension
