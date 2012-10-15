9dbc88eeecdfa0bb123d865f19c1bd0e0596e1df
Filip Lundgren <filip@inkdev.net>
2012-10-15 03:08:05 +0200

    CActor::ProcessEvent: Reset anim character state on entity init.

--- MonoDll/Actor.cpp
+++ MonoDll/Actor.cpp
@@ -154,6 +154,12 @@ void CActor::ProcessEvent(SEntityEvent& event)
 	case ENTITY_EVENT_PREPHYSICSUPDATE:
 		m_pScript->CallMethod("OnPrePhysicsUpdate");
 		break;
+	case ENTITY_EVENT_INIT:
+		{
+			if (m_pAnimatedCharacter)
+				m_pAnimatedCharacter->ResetState();
+		}
+		break;
   }  
 }
 
