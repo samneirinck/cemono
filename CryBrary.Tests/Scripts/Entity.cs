7a793f129d73e4a66da651bd7ae66d4c1ba0548e
Filip Lundgren <filip@inkdev.net>
2012-10-18 13:13:11 +0200

    Added more Actor / Entity script tests

--- CryBrary.Tests/Scripts/Entity.cs
+++ CryBrary.Tests/Scripts/Entity.cs
@@ -41,6 +41,36 @@ namespace CryBrary.Tests.Scripts
 		}
 
 		[Fact]
+		public void Get_Mono_Entity_By_EntityId_As_BaseType()
+		{
+			var scriptManager = new ScriptManager();
+			ScriptManager.Instance = scriptManager;
+
+			var entity = new MyEntity();
+			entity.InternalSpawn(new EntityInfo { Id = 19 });
+			scriptManager.AddScriptInstance(entity, ScriptType.Entity);
+
+			var retrievedEntity = Entity.Get<Entity>(19);
+			Assert.NotNull(retrievedEntity);
+			Assert.Same(retrievedEntity, entity);
+		}
+
+		[Fact]
+		public void Get_Mono_Entity_By_EntityId_As_BaseType2()
+		{
+			var scriptManager = new ScriptManager();
+			ScriptManager.Instance = scriptManager;
+
+			var entity = new MyEntity();
+			entity.InternalSpawn(new EntityInfo { Id = 27 });
+			scriptManager.AddScriptInstance(entity, ScriptType.Entity);
+
+			var retrievedEntity = Entity.Get<EntityBase>(27);
+			Assert.NotNull(retrievedEntity);
+			Assert.Same(retrievedEntity, entity);
+		}
+
+		[Fact]
 		public void Get_Native_Entity_By_EntityId_Generic()
 		{
 			var scriptManager = new ScriptManager();
