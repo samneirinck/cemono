7712ef53a8f10bb4271f823f831a881dd248d5c0
Filip Lundgren <filip@inkdev.net>
2012-10-18 17:47:48 +0200

    Register actors.

--- CryBrary/Native/Interfaces/INativeActorMethods.cs
+++ CryBrary/Native/Interfaces/INativeActorMethods.cs
@@ -10,6 +10,7 @@ namespace CryEngine.Native
         void SetPlayerMaxHealth(IntPtr actorPtr, float newMaxHealth);
         ActorInfo GetActorInfoByChannelId(ushort channelId);
         ActorInfo GetActorInfoById(uint entId);
+		void RegisterClass(string name, bool isNative);
         ActorInfo CreateActor(Actor actor, int channelId, string name, string className, Vec3 pos, Quat rot, Vec3 scale);
         void RemoveActor(uint id);
         uint GetClientActorId();
