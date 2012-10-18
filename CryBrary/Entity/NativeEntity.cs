a851c3c073f79dbcd76b849911a5a3865ac72c89
Filip Lundgren <filip@inkdev.net>
2012-10-18 21:51:28 +0200

    Fix: NativeEntity has to derive from Entity. (Used for attachments and network entity)

--- CryBrary/Entity/NativeEntity.cs
+++ CryBrary/Entity/NativeEntity.cs
@@ -9,7 +9,7 @@ namespace CryEngine
 	/// Represents an entity registered outside of CryMono, e.g. in CryGame.dll.
 	/// </summary>
 	[ExcludeFromCompilation]
-	internal class NativeEntity : EntityBase
+	internal class NativeEntity : Entity
 	{
 		public NativeEntity() { }
 		public NativeEntity(EntityId id, IntPtr ptr)
