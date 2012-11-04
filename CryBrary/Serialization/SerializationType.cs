9df13c1b7b0f3c82e418cde54187993e297648e1
i59 <filip@inkdev.net>
2012-11-04 02:42:51 +0100

    CrySerializer: Better way of determining serialization type.

---
+++ CryBrary/Serialization/SerializationType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine.Serialization
+{
+    enum SerializationType
+    {
+        Null,
+        Reference,
+        Object,
+        GenericEnumerable,
+        Enumerable,
+        Enum,
+        Any,
+        String,
+        MemberInfo,
+        Type,
+        Delegate,
+        IntPtr
+    }
+}
--- CryBrary/Serialization/SerializedObjectReference.cs
+++ CryBrary/Serialization/SerializedObjectReference.cs
@@ -1,4 +1,9 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Reflection;
+
+using CryEngine.Extensions;
+
 namespace CryEngine.Serialization
 {
     class ObjectReference
@@ -11,10 +16,41 @@ namespace CryEngine.Serialization
         {
             Name = name;
             Value = value;
+
+            Type valueType = Value != null ? Value.GetType() : null;
+            if (valueType == null || valueType.IsPointer)
+                SerializationType = SerializationType.Null;
+            else if (valueType == typeof(IntPtr))
+                SerializationType = SerializationType.IntPtr;
+            else if (valueType.IsPrimitive)
+                SerializationType = SerializationType.Any;
+            else if (valueType == typeof(string))
+                SerializationType = SerializationType.String;
+            else if (valueType.Implements<IEnumerable>())
+            {
+                if (valueType.IsGenericType)
+                    SerializationType = SerializationType.GenericEnumerable;
+                else
+                    SerializationType = SerializationType.Enumerable;
+            }
+            else if (valueType.IsEnum)
+                SerializationType = SerializationType.Enum;
+            else
+            {
+                if (Value is Type)
+                    SerializationType = SerializationType.Type;
+                else if (valueType.Implements<Delegate>())
+                    SerializationType = SerializationType.Delegate;
+                else if (valueType.Implements<MemberInfo>())
+                    SerializationType = SerializationType.MemberInfo;
+                else
+                    SerializationType = SerializationType.Object;
+            }
         }
 
         public string Name { get; set; }
         public object Value { get; set; }
+        public SerializationType SerializationType { get; set; }
 
         public bool AllowNull { get; set; }
     }
