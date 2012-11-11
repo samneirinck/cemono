2f8de8a6f866f0eb0a424b6fef2f58312f42f04e
i59 <filip@inkdev.net>
2012-11-11 06:31:06 +0100

    Fix: Input events not triggered after reload.

---
+++ CryBrary/Input/Input.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace CryEngine
+{
+    public delegate void ActionMapEventDelegate(ActionMapEventArgs e);
+
+    public delegate void KeyEventDelegate(KeyEventArgs e);
+
+    public delegate void MouseEventDelegate(MouseEventArgs e);
+
+    public static class Input
+    {
+        #region Events
+        static void OnActionTriggered(string action, KeyEvent keyEvent, float value)
+        {
+            ActionmapEvents.Invoke(new ActionMapEventArgs(keyEvent, action, value));
+        }
+
+        static void OnKeyEvent(string keyName, float value)
+        {
+            if (KeyEvents != null)
+                KeyEvents(new KeyEventArgs(keyName, value));
+        }
+
+        static void OnMouseEvent(int x, int y, MouseEvent mouseEvent, int wheelDelta)
+        {
+            MouseX = x;
+            MouseY = y;
+
+            if (MouseEvents != null)
+                MouseEvents(new MouseEventArgs(x, y, wheelDelta, mouseEvent));
+        }
+
+        public static int MouseX { get; set; }
+
+        public static int MouseY { get; set; }
+        #endregion
+
+        public static event KeyEventDelegate KeyEvents;
+        internal static Delegate[] KeyEventsInvocationList { get { return KeyEvents != null ? KeyEvents.GetInvocationList() : null; } }
+
+        public static event MouseEventDelegate MouseEvents;
+        internal static Delegate[] MouseEventsInvocationList { get { return MouseEvents != null ? MouseEvents.GetInvocationList() : null; } }
+
+        public static ActionmapHandler ActionmapEvents = new ActionmapHandler();
+
+        static void OnScriptInstanceDestroyed(CryScriptInstance instance)
+        {
+            foreach (KeyEventDelegate d in KeyEvents.GetInvocationList())
+            {
+                if (d.Target == instance)
+                    KeyEvents -= d;
+            }
+
+            foreach (MouseEventDelegate d in MouseEvents.GetInvocationList())
+            {
+                if (d.Target == instance)
+                    MouseEvents -= d;
+            }
+
+            ActionmapEvents.RemoveAll(instance);
+        }
+    }
+}
---
+++ CryBrary/Input/SerializableInput.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CryEngine
+{
+    /// <summary>
+    /// Used to cross over input data for realtime scripting.
+    /// </summary>
+    internal class SerializableInput
+    {
+        public Delegate[] KeyEvents { get; set; }
+        public Delegate[] MouseEvents { get; set; }
+
+        public ActionmapHandler ActionmapEvents { get; set; }
+    }
+}
