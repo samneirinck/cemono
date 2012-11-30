76fa25fc0c129752f4df931ab9f5b2a67d43fa6c
i59 <filip@inkdev.net>
2012-11-30 23:11:31 +0100

    .gitignore: Exclude CryBrary/Debug.

---
+++ CryBrary/Debug/Native/NativeDebugMethods.cs
@@ -0,0 +1,38 @@
+﻿using System.Runtime.CompilerServices;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+using System.Security;
+
+namespace CryEngine.Native
+{
+    internal static class NativeDebugMethods
+    {
+        #region Persistent Debug
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern internal static void AddPersistentSphere(Vec3 pos, float radius, Color color, float timeout);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern internal static void AddDirection(Vec3 pos, float radius, Vec3 dir, Color color, float timeout);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern internal static void AddPersistentText2D(string text, float size, Color color, float timeout);
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern internal static void AddPersistentLine(Vec3 pos, Vec3 end, Color color, float timeout);
+        #endregion
+
+        #region Logging
+        [SuppressUnmanagedCodeSecurity]
+        [SuppressMessage("Microsoft.Globalization", "CA2101:SpecifyMarshalingForPInvokeStringArguments", MessageId = "0"), DllImport("CryMono.dll")]
+        public extern static void LogAlways(string msg);
+
+        [SuppressUnmanagedCodeSecurity]
+        [SuppressMessage("Microsoft.Globalization", "CA2101:SpecifyMarshalingForPInvokeStringArguments", MessageId = "0"), DllImport("CryMono.dll")]
+        public extern static void Log(string msg);
+
+        [SuppressUnmanagedCodeSecurity]
+        [SuppressMessage("Microsoft.Globalization", "CA2101:SpecifyMarshalingForPInvokeStringArguments", MessageId = "0"), DllImport("CryMono.dll")]
+        public extern static void Warning(string msg);
+        #endregion
+    }
+}
