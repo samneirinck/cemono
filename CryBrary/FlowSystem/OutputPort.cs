704c85091c138aba80fd0d6d9ca296a9565d4333
i59 <filip@inkdev.net>
2013-02-26 04:46:00 +0100

    FlowSystem.OutputPort: Added Activate(object) method.

--- CryBrary/FlowSystem/OutputPort.cs
+++ CryBrary/FlowSystem/OutputPort.cs
@@ -53,6 +53,24 @@ namespace CryEngine.FlowSystem
             NativeFlowNodeMethods.ActivateOutputVec3(ParentNodePointer, PortId, value);
         }
 
+        public void Activate(object value)
+        {
+            if (value is int)
+                NativeFlowNodeMethods.ActivateOutputInt(ParentNodePointer, PortId, System.Convert.ToInt32(value));
+            else if (value is float || value is double)
+                NativeFlowNodeMethods.ActivateOutputFloat(ParentNodePointer, PortId, System.Convert.ToSingle(value));
+            else if (value is EntityId)
+                NativeFlowNodeMethods.ActivateOutputEntityId(ParentNodePointer, PortId, (uint)((EntityId)(object)value)._value);
+            else if (value is string)
+                NativeFlowNodeMethods.ActivateOutputString(ParentNodePointer, PortId, System.Convert.ToString(value));
+            else if (value is bool)
+                NativeFlowNodeMethods.ActivateOutputBool(ParentNodePointer, PortId, System.Convert.ToBoolean(value));
+            else if (value is Vec3)
+                NativeFlowNodeMethods.ActivateOutputVec3(ParentNodePointer, PortId, (Vec3)(object)value);
+            else
+                throw new ArgumentException("Attempted to activate output with invalid value type!");
+        }
+
         IntPtr ParentNodePointer { get; set; }
 
         int PortId { get; set; }
@@ -87,7 +105,7 @@ namespace CryEngine.FlowSystem
             else if (value is Vec3)
                 NativeFlowNodeMethods.ActivateOutputVec3(ParentNodePointer, PortId, (Vec3)(object)value);
             else
-                throw new ArgumentException("Attempted to activate output with invalid value!");
+                throw new ArgumentException("Attempted to activate output with invalid value type!");
         }
 
         IntPtr ParentNodePointer { get; set; }
