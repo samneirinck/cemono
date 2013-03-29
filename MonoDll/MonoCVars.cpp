59cd50c0483788348a8b9d5b8a7f6cec55e83fa9
i59 <filip@inkdev.net>
2013-03-29 19:24:35 +0100

    Mono CVars: Set some CVasrs with the VF_CHEAT flag in release mode.

--- MonoDll/MonoCVars.cpp
+++ MonoDll/MonoCVars.cpp
@@ -10,17 +10,23 @@ void CmdReload(IConsoleCmdArgs *pCmdArgs)
 
 void SCVars::InitCVars(IConsole *pConsole)
 {
+	EVarFlags nullOrCheatFlag = VF_NULL;
+#ifdef RELEASE
+	nullOrCheatFlag = VF_CHEAT;
+#endif
+
 	REGISTER_CVAR(mono_exceptionsTriggerMessageBoxes, 1, VF_NULL, "If true, exceptions will trigger a message box to appear");
 	REGISTER_CVAR(mono_exceptionsTriggerFatalErrors, 0, VF_NULL, "If true, exceptions will trigger a fatal error");
 
-	REGISTER_CVAR(mono_realtimeScripting, 1, VF_NULL, "Enables / Disables Realtime Scripting functionality. (Editor-only)");
+	REGISTER_CVAR(mono_realtimeScripting, 1, nullOrCheatFlag, "Enables / Disables Realtime Scripting functionality. (Editor-only)");
 	
 #ifndef RELEASE
-	REGISTER_CVAR(mono_realtimeScriptingDebug, 0, VF_NULL, "Toggles on realtime scripting debug, useful for finding serialization bugs");
+	REGISTER_CVAR(mono_realtimeScriptingDebug, 0, nullOrCheatFlag, "Toggles on realtime scripting debug, useful for finding serialization bugs");
 #endif
 
-	REGISTER_CVAR(mono_realtimeScriptingDetectChanges, 1, VF_NULL, "Toggles whether realtime scripting should automatically reload scripts when any *.cs file is modified in the scripts directory.");
-	REGISTER_COMMAND("mono_reload", CmdReload, VF_NULL, "[Realtime Scripting] Reloads / recompiles all scripts");
+	REGISTER_CVAR(mono_realtimeScriptingDetectChanges, 1, nullOrCheatFlag, "Toggles whether realtime scripting should automatically reload scripts when any *.cs file is modified in the scripts directory.");
+
+	REGISTER_COMMAND("mono_reload", CmdReload, nullOrCheatFlag, "[Realtime Scripting] Reloads / recompiles all scripts");
 
 	REGISTER_CVAR(mono_softBreakpoints, 1, VF_REQUIRE_APP_RESTART, "[Performance Warning] Enables / Disables soft breakpoints, preventing managed null reference exceptions causing crashes in unmanaged code. \n Not supported along with -DEBUG command line option");
 }
